cc695c1e91ae73d17cc5bad530f3cd4aa2b1403e
Ariel Chen <ariel7234@gmail.com>
2023-01-28 18:34:14 +0800

    fix: 將每週任務搬到同一個repo

---
+++ week2/login.js
@@ -0,0 +1,29 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+
+createApp({
+  data() {
+    return {
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`${this.apiUrl}/admin/signin`, this.user)
+        .then((res) => {
+          const { token, expired } = res.data;
+          // token 寫入 cookie
+          document.cookie = `hexToken=${token}; expires=${expired};`;
+          window.location = "products.html";
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        })
+    },
+  },
+}).mount("#app");
